4c2e2b5ed1e249899885e7484295a726b63ab7d0
John Stempowski <johnstempowski@quickenloans.com>
2020-02-07 07:55:45 -0500

    This is my initial commit

---
+++ Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab4_2
+{
+    class Movie
+    {
+        #region fields
+        private string title;
+        private string category;
+        #endregion fields
+
+        #region properties
+        public string Title { get => title; set => title = value; }
+        public string Category { get => category; set => category = value; }
+        #endregion properties
+
+
+        #region constructor
+        public Movie(string title, string category)
+        {
+            this.title = title;
+            this.category = category;
+        }
+        #endregion constructor
+
+        #region methods
+        public override string ToString()
+        {
+            return $"{title}";
+        }
+
+        #endregion methods
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string genre = GetInput("Please pick a genre: ");
+                FindMovies(genre);
+            } while (GoAgain());
+            Console.WriteLine("Thank you for your interest!");
+            
+            
+            static void FindMovies(string genre)
+            {
+                List<Movie> movies = new List<Movie>();
+                movies.Add(new Movie("Cars 3", "animated"));
+                movies.Add(new Movie("Star Wars", "scifi"));
+                movies.Add(new Movie("Dracula", "horror"));
+                movies.Add(new Movie("Star Trek II", "scifi"));
+                movies.Add(new Movie("Pokemon the Movie", "animated"));
+                movies.Add(new Movie("Field of Dreams", "drama"));
+                movies.Add(new Movie("Birds", "horror"));
+                movies.Add(new Movie("Toy Story", "animated"));
+                movies.Add(new Movie("Iron Man", "scifi"));
+                movies.Add(new Movie("Ready Player One", "scifi"));
+
+                foreach (Movie m in movies)
+                {
+                    if (m.Category == genre)
+                    {
+                        Console.WriteLine(m);
+                    }
+                }
+            }
+
+            static bool GoAgain()
+            {
+                char c;
+                do
+                {
+                    Console.Write("Would you like to search another genre or category? (y/n) ");
+                    c = Console.ReadKey().KeyChar;
+
+                    if (c == 'n')
+                    {
+                        return false;
+                    }
+                } while (c != 'y');
+
+                return true;
+            }
+
+            static string GetInput(string prompt)
+            {
+                string input;
+
+                do
+                {
+                    Console.Write(prompt);
+                    input = Console.ReadLine();
+                } while (input != "animated" && input != "drama" && input != "scifi" && input != "horror");
+                return input;
+
+
+                
+                
+            }
+
+        }
+    }
+}
